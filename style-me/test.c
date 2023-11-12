6da89217641cbc51f6e253425e7e71a311f5cf29
Shariar Papon <spaponwork@gmail.com>
2023-11-12 16:49:13 -0500

    flags added for debugging

--- style-me/test.c
+++ style-me/test.c
@@ -2,23 +2,33 @@
 #include "TOKENIZER.h"
 #include <stdio.h>
 
+#define PRINT_SORUCE 1
+#define PRINT_TOKENIZED_OUTPUT 0
 
 int main()
 {
 	const char* test_source_file = "testsource.txt";
 	const char* test_source = read_source_file(test_source_file);
-	printf("SOURCE CODE\n----------------\n");
-	printf("%s%s", test_source,"\n-------------------------------------\n\n");
 
-	Tokenizer* tokenizer = create_tokenizer(test_source);
-	TokenizerOutput output = tokenize(tokenizer);
-	for (int i = 0; i < output.count; ++i)
+	if (PRINT_SORUCE) 
 	{
-		const char* kind_str = tokenkind_to_str(output.tokens[i].kind);
-		const char* token_val = get_token_value(output.tokens[i]);
-		printf("%s  %s\n", kind_str, token_val);
+		printf("%s%s%s","-------------------\nSOURCE CODE\n-------------------\n", test_source, "\n");
 	}
-	free_tokenizer(tokenizer);
-	free_tokenizer_output(output);
+
+	if (PRINT_TOKENIZED_OUTPUT)
+	{
+		printf("-------------------\nTOKENIZED OUTPUT\n-------------------\n");
+		Tokenizer* tokenizer = create_tokenizer(test_source);
+		TokenizerOutput output = tokenize(tokenizer);
+		for (int i = 0; i < output.count; ++i)
+		{
+			const char* kind_str = tokenkind_to_str(output.tokens[i].kind);
+			const char* token_val = get_token_value(output.tokens[i]);
+			printf("%s  %s\n", kind_str, token_val);
+		}
+		free_tokenizer(tokenizer);
+		free_tokenizer_output(output);
+	}
+
 	return 0;
 }
